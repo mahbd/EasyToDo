3d3547e8230a5f806077d9e40d18ddc01741febe
Mahmudul Alam <mahmudula2000@gmail.com>
2023-05-12 13:17:02 +0600

    configured realm

---
+++ app/src/main/java/com/example/easytodo/MainApplication.java
@@ -0,0 +1,23 @@
+package com.example.easytodo;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder()
+                .name("easy_todo.realm")
+                .schemaVersion(1)
+                .allowWritesOnUiThread(true)
+                .allowQueriesOnUiThread(true)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
