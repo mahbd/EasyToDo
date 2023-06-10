cc46ee468b780ec31125751590c35982b9179fda
Mahmudul Alam <mahmudula2000@gmail.com>
2023-06-10 08:49:39 +0600

    solved crash while deleting tag and project

--- app/src/main/java/com/example/easytodo/ui/screens/TagScreen.java
+++ app/src/main/java/com/example/easytodo/ui/screens/TagScreen.java
@@ -56,7 +56,6 @@ public class TagScreen extends Fragment {
                     Tag tag = tags.get(position);
                     if (tag != null) {
                         Tag.delete(tag.getId());
-                        requireActivity().recreate();
                     }
                 }
                 return true;
